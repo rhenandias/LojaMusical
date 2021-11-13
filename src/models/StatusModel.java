3b794e98539cd3c004df2dd8b382cb6ab57c76b3
João <joaovictor1212@example.com>
2021-11-13 15:40:57 -0300

    :sparkles: Variaveis

--- src/models/StatusModel.java
+++ src/models/StatusModel.java
@@ -1,5 +1,20 @@
 package models;
 
 public class StatusModel {
-
+	private int id;
+	private String nome;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
