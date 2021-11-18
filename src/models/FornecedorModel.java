2b14ea8114f5bd71040b36fe2e1a465e7c9e044b
João <joaovictor1212@example.com>
2021-11-18 20:56:10 -0300

    :sparkles: Atualizações nos modulos

--- src/models/FornecedorModel.java
+++ src/models/FornecedorModel.java
@@ -9,8 +9,25 @@ public class FornecedorModel {
 	private String endereco;
 	private String cep;
 	private String uf;
-	private String cidFornecedorade;
+	private String cidade;
 	private String telefone;
+	
+	public FornecedorModel() { }
+	
+	public FornecedorModel(int idFornecedor, String nome, String cpfcnpj, String email, String endereco, String cep, String uf, String cidade, String telefone) {
+		super();
+		this.idFornecedor = idFornecedor;
+		this.nome = nome;
+		this.cpfcnpj = cpfcnpj;
+		this.email = email;
+		this.endereco = endereco;
+		this.cep = cep;
+		this.uf = uf;
+		this.cidade = cidade;
+		this.telefone = telefone;
+		
+	}
+	
 	
 	public int getidFornecedor() {
 		return idFornecedor;
@@ -54,11 +71,11 @@ public class FornecedorModel {
 	public void setUf(String uf) {
 		this.uf = uf;
 	}
-	public String getCidFornecedorade() {
-		return cidFornecedorade;
+	public String getCidade() {
+		return cidade;
 	}
-	public void setCidFornecedorade(String cidFornecedorade) {
-		this.cidFornecedorade = cidFornecedorade;
+	public void setCidFornecedorade(String cidade) {
+		this.cidade = cidade;
 	}
 	public String getTelefone() {
 		return telefone;
--- src/models/StatusModel.java
+++ src/models/StatusModel.java
@@ -4,6 +4,14 @@ public class StatusModel {
 	private int idStatus;
 	private String nome;
 	
+	public StatusModel() { }
+		
+	public StatusModel(int idStatus, String nome) {
+		super();
+		this.idStatus = idStatus;
+		this.nome = nome;
+	}
+	
 	public int getidStatus() {
 		return idStatus;
 	}
