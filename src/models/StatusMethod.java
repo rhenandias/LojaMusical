868f565f364c00e6550ed38c67f2132729efafd6
Samuel de Lima Capusesera <samueldelimapessoal@gmail.com>
2021-11-23 14:03:57 -0300

    Criacao de um classe de retorno de status

---
+++ src/models/StatusMethod.java
@@ -0,0 +1,55 @@
+package models;
+
+import models.enums.STATUS;
+
+public class StatusMethod {
+	
+	public STATUS status;
+	public String Titulo;
+	public String mensagem;
+
+	
+	public StatusMethod() {
+	}
+
+	public StatusMethod(STATUS status, String titulo, String mensagem) {
+		super();
+		this.status = status;
+		Titulo = titulo;
+		this.mensagem = mensagem;
+	}
+
+	public STATUS getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(STATUS status) {
+		this.status = status;
+	}
+
+
+	public String getTitulo() {
+		return Titulo;
+	}
+
+
+	public void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/models/enums/STATUS.java
@@ -0,0 +1,7 @@
+package models.enums;
+
+public enum STATUS {
+	SUCCESS,
+	ERROR,
+	INFO,
+}
