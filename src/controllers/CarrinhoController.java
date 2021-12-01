768da458433e466b2c9e5304b4f85e26aa2e15f2
Samuel de Lima Capusesera <samueldelimapessoal@gmail.com>
2021-11-30 23:13:16 -0300

    Criação do controller 'carrinho' para armazenar via cookie

--- src/controllers/CarrinhoController.java
+++ src/controllers/CarrinhoController.java
@@ -1,15 +1,21 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/carrinho")
+
+import com.google.gson.JsonObject;
+
+@WebServlet("/carrinho/*")
 public class CarrinhoController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -17,15 +23,54 @@ public class CarrinhoController extends HttpServlet {
         super();
     }
 
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher tagFile = null;
+		String uri = request.getRequestURI();
 		tagFile = getServletContext().getRequestDispatcher("/View/carrinho.jsp");
+		
+		String action = uri.substring(uri.lastIndexOf("/") + 1);
+		
+		switch (action) {
+			case "inserirProduto" : {
+				try {
+					String idProduto = request.getParameter("idProduto");
+					String quantidade = request.getParameter("quantidade");
+					Date date = new Date();
+					String cookieName = "carrinho" + "_id-" + idProduto + "_qtd-" + quantidade + "_" + String.valueOf(date.hashCode());
+					Cookie cookie = new Cookie(cookieName, idProduto);
+					cookie.setMaxAge(60*60*24);
+					cookie.setSecure(true);
+					response.addCookie(cookie);
+					
+					
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+			default:
+				Cookie cookies[] = request.getCookies();
+				break;
+				
+				
+				
+		}
+		
+		
 		tagFile.forward(request, response);
+		//JsonObject innerObject = new JsonObject();
+		//innerObject.addProperty("idProduto", query.getString("idProduto"));
+		
+		
+
+		
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
 		doGet(request, response);
 	}
 
